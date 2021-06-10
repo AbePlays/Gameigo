6a2034c012711db42fa0884fd6c611dd17c6b2ce
Abe <abhi.rawat456@gmail.com>
2021-06-10 19:45:50 +0530

    feat: added auth container

---
+++ containers/Auth/Login.tsx
@@ -0,0 +1,88 @@
+import { FunctionComponent } from 'react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { Box, Heading, Stack, Text } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+
+import { LoginForm } from './types';
+import { checkEmail, checkPassword } from './helper';
+import { BrandButton } from '../../components/Buttons';
+import { CustomInput } from '../../components/Input';
+
+const Login: FunctionComponent = () => {
+  const initialValues: LoginForm = { email: '', password: '' };
+
+  return (
+    <Box textAlign="center">
+      <Heading as="h1" fontWeight="extrabold" fontSize={['5xl', '6xl']}>
+        Login to Your Account
+      </Heading>
+      <Text fontSize="xl" maxW="xl" mx="auto" color="gray.700" mt="8">
+        Choose from 30,000+ games around the world with new addition every few
+        weeks
+      </Text>
+      <Box
+        display="flex"
+        flexDirection={['column', 'column', 'row']}
+        justifyContent="center"
+        mt={['10', '10', '32']}
+        maxW={['full', '30rem', '50rem']}
+        mx="auto"
+        style={{ gap: '5rem' }}
+      >
+        <Box flex="1">
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                actions.setSubmitting(false);
+              }, 1000);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Stack spacing="4">
+                  <CustomInput
+                    name="email"
+                    type="text"
+                    placeholder="Enter your email"
+                    validate={checkEmail}
+                  />
+                  <CustomInput
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    validate={checkPassword}
+                  />
+                  <Button
+                    h="12"
+                    bg="black"
+                    color="white"
+                    _hover={{
+                      bg: '#444',
+                    }}
+                    isLoading={props.isSubmitting}
+                    type="submit"
+                    rightIcon={<ChevronRightIcon h="6" w="6" />}
+                    justifyContent={
+                      props.isSubmitting ? 'center' : 'space-between'
+                    }
+                  >
+                    Login to Your Account
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+        <Stack spacing="4" flex="1">
+          <BrandButton imgSrc="/icons/google.svg" title="Sign in with Google" />
+          <BrandButton imgSrc="/icons/github.svg" title="Sign in with Github" />
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
---
+++ utils/auth.ts
@@ -0,0 +1,10 @@
+export const validateEmail = (email: string): boolean => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+export const validatePassword = (password: string): boolean => {
+  if (password.trim().length < 6) return false;
+  return true;
+};
