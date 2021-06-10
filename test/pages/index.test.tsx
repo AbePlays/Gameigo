90a03521397ecc07d26c0518906e436d327df24f
Abe <abhi.rawat456@gmail.com>
2021-06-10 11:19:39 +0530

    test: first test

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,10 +1,29 @@
-import Home from "../../pages/index";
+import Home from '../../pages/index';
+import { Routes } from '../../routes';
+import { render, screen, fireEvent } from '../test-utils';
 
-import { render, screen } from "../test-utils";
+const mockPush = jest.fn();
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    push: mockPush,
+  })),
+}));
 
-describe("Home Page", () => {
-  it("contains nextjs", () => {
+describe('Testing Home Component', () => {
+  it('should check for hero-image', () => {
     render(<Home />);
-    expect(screen.getByText(/hello, nextjs/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('img', { name: /man playing on his laptop/i })
+    ).toBeInTheDocument();
+  });
+
+  it('should check CTA', () => {
+    render(<Home />);
+    const button = screen.getByRole('button', { name: /get started/i });
+    expect(button).toBeInTheDocument();
+
+    fireEvent.click(button);
+    expect(mockPush).toHaveBeenCalledWith(Routes.AUTH_SCREEN);
+    expect(mockPush).toHaveBeenCalledTimes(1);
   });
 });
