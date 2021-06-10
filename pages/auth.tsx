0ccd4b0a9665306e7e26c294e4b17a45bef2a280
Abe <abhi.rawat456@gmail.com>
2021-06-10 19:46:09 +0530

    feat: auth page added

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,31 @@
-import { Box } from '@chakra-ui/layout';
+import { FunctionComponent, useState } from 'react';
+import { Button } from '@chakra-ui/button';
+import { Box, Text } from '@chakra-ui/layout';
 
-interface Props {}
+import { Login, Signup } from '../containers/Auth';
 
-const auth = (props: Props) => {
-  return <Box>Yo</Box>;
+const Auth: FunctionComponent = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+
+  const footer = {
+    head: isLogin ? "Don't have an account? " : 'Already have an account? ',
+    tail: isLogin ? 'Sign up' : 'Log in',
+  };
+
+  const toggle = () => setIsLogin((prev) => !prev);
+
+  return (
+    <Box textAlign="center">
+      {isLogin ? <Login /> : <Signup />}
+      <Text mt={['10', '10', '16']}>
+        {footer.head}
+        <Button variant="link" color="black" onClick={toggle}>
+          {footer.tail}
+        </Button>{' '}
+        instead
+      </Text>
+    </Box>
+  );
 };
 
-export default auth;
+export default Auth;
