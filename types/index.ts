cadd8cc0c3ba028bd068afa7f46dcdf601bd39c2
Abe <abhi.rawat456@gmail.com>
2021-06-12 20:54:50 +0530

    feat: data fetching added

---
+++ types/index.ts
@@ -0,0 +1,20 @@
+export type Platform = {
+  id: number;
+  slug: string;
+  name: string;
+};
+
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+export type Game = {
+  slug: string;
+  id: number;
+  name: string;
+  released: string;
+  background_image: string;
+  parent_platforms: Platform[];
+  genres: Genre[];
+};
