6a1ceb52ed9a1acde6a6102bd8d18a34d3d9adf6
Abe <abhi.rawat456@gmail.com>
2021-06-12 21:17:11 +0530

    fix: changed date interval

--- endpoints.ts
+++ endpoints.ts
@@ -2,5 +2,5 @@ const baseUrl = 'https://api.rawg.io/api';
 const apiKey = `?key=${process.env.NEXT_PUBLIC_RAWG_API_KEY}`;
 
 export const Endpoints = {
-  TRENDING_GAMES: `${baseUrl}/games${apiKey}&dates=2021-10-10,2021-10-10&ordering=-added`,
+  TRENDING_GAMES: `${baseUrl}/games${apiKey}&dates=2020-10-10,2020-10-10&ordering=-added`,
 };
