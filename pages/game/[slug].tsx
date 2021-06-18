b4ecc04de54fc9e45cae9a0711d1fed246086834
Abe <abhi.rawat456@gmail.com>
2021-06-18 20:46:04 +0530

    feat: added route to show game details

---
+++ pages/game/[slug].tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent } from 'react';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+
+import GameDetail from '@/containers/GameDetail';
+import { Endpoints } from 'endpoints';
+import { GameInfo } from 'types';
+
+interface Props {
+  game: GameInfo;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { slug: 'cyberpunk-2077' } }],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const { slug } = context.params;
+  const result = await fetch(
+    `${Endpoints.SEARCH_GAME}/${slug}?key=${process.env.NEXT_PUBLIC_RAWG_API_KEY}`
+  );
+  const data = await result.json();
+
+  return { props: { game: data } };
+};
+
+const GameDetailContainer: FunctionComponent<Props> = ({ game }) => {
+  return <GameDetail game={game} />;
+};
+
+export default GameDetailContainer;
