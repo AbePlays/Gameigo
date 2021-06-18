10deebb971667836fd02d66c92ecdd161bdb1bb6
Abe <abhi.rawat456@gmail.com>
2021-06-18 20:44:41 +0530

    chore: searching game endpoint added

--- endpoints.ts
+++ endpoints.ts
@@ -3,4 +3,5 @@ const apiKey = `?key=${process.env.NEXT_PUBLIC_RAWG_API_KEY}`;
 
 export const Endpoints = {
   TRENDING_GAMES: `${baseUrl}/games${apiKey}&dates=2020-06-12,2021-06-12&ordering=-added`,
+  SEARCH_GAME: `${baseUrl}/games`,
 };
