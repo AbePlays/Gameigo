e73bef7351be2f429f9825f24356f0075a611af4
Abe <abhi.rawat456@gmail.com>
2021-07-04 18:02:06 +0530

    feat: firebase-admin setup

---
+++ lib/db-admin.ts
@@ -0,0 +1,8 @@
+import { db } from './firebase-admin';
+
+export const getUserData = async (
+  uid: string
+): Promise<FirebaseFirestore.DocumentData> => {
+  const docs = await db.collection('users').doc(uid).get();
+  return docs.data();
+};
