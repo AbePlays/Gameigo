319071ef55f9d043f5f74ca69ee6d3aec5c9c55e
Abe <abhi.rawat456@gmail.com>
2021-08-09 18:55:57 +0530

    fix: using getApps instead of try-catch

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,14 @@
-import { initializeApp } from 'firebase/app';
+import { getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 let firebaseApp;
-try {
+if (getApps().length < 1) {
   firebaseApp = initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   });
-} catch (e) {
-  console.log(e);
 }
 
 const auth = getAuth(firebaseApp);
