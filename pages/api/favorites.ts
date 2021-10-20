e7dc67d57f55e2ef1569f9c192649b3b6e28e699
Abe <abhi.rawat456@gmail.com>
2021-10-20 22:39:34 +0530

    feat: fetching favorites data on client

--- pages/api/favorites.ts
+++ pages/api/favorites.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import { auth } from 'lib/firebase-admin';
 import { getUserData } from 'lib/db-admin';
 
 export default async (
@@ -7,12 +8,13 @@ export default async (
   res: NextApiResponse
 ): Promise<unknown> => {
   try {
-    const { uid } = req.headers;
-    if (typeof uid === 'string') {
+    const { token } = req.headers;
+    if (typeof token === 'string') {
+      const { uid } = await auth.verifyIdToken(token);
       const { favorites } = await getUserData(uid);
       return res.status(200).json(favorites);
     }
-    res.status(500).json({ error: 'invalid uid' });
+    res.status(500).json({ error: 'invalid token' });
   } catch (error) {
     res.status(500).json({ error });
   }
