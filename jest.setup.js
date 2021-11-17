570c87efebe58b22eba45acba32c058a5ca89dd7
Abe <abhi.rawat456@gmail.com>
2021-11-17 17:56:41 +0530

    chore: updated packages and config files

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,9 @@
 import '@testing-library/jest-dom/extend-expect';
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
+
+jest.mock('firebase/firestore', () => {
+  return {
+    getFirestore: jest.fn(),
+  };
+});
