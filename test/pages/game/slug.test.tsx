9e26ddf7644c1484fc937a9b3bb0939c58f5325c
Abe <abhi.rawat456@gmail.com>
2021-11-24 17:33:36 +0530

    test: more tests

---
+++ test/pages/game/slug.test.tsx
@@ -0,0 +1,44 @@
+import GameDetailContainer, {
+  getStaticPaths,
+  getStaticProps,
+} from 'pages/game/[slug]';
+import { mockGame, mockGameInfo, mockScreenshots } from '../../mockData';
+import { render, screen } from '../../test-utils';
+
+describe('Testing Game Details Page', () => {
+  it('should check game title', () => {
+    render(
+      <GameDetailContainer
+        data={{ game: mockGameInfo, screenshots: mockScreenshots }}
+      />
+    );
+
+    expect(screen.getByText(mockGameInfo.name)).toBeInTheDocument();
+  });
+
+  it('should check getStaticProps', async () => {
+    const response = await getStaticProps({
+      params: { slug: 'forza-horizon-5' },
+    });
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          data: {
+            game: mockGameInfo,
+            screenshots: mockScreenshots,
+          },
+        },
+      })
+    );
+  });
+
+  it('should check getStaticPaths', async () => {
+    const response = await getStaticPaths(null);
+    expect(response).toEqual(
+      expect.objectContaining({
+        paths: [{ params: { slug: mockGame.slug } }],
+        fallback: 'blocking',
+      })
+    );
+  });
+});
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,15 +1,21 @@
 import Home from '../../pages/index';
 import { Routes } from '../../routes';
-import { render, screen, fireEvent } from '../test-utils';
+import { fireEvent, mockValue, mockUser, render, screen } from '../test-utils';
 
 const mockPush = jest.fn();
+const mockReplace = jest.fn();
 jest.mock('next/router', () => ({
   useRouter: jest.fn().mockImplementation(() => ({
     push: mockPush,
+    replace: mockReplace,
   })),
 }));
 
 describe('Testing Home Component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should check CTA', async () => {
     render(<Home />);
     const buttonEls = await screen.findAllByRole('button', {
@@ -20,5 +26,17 @@ describe('Testing Home Component', () => {
     fireEvent.click(buttonEls[0]);
     expect(mockPush).toHaveBeenCalledWith(Routes.AUTH_SCREEN);
     expect(mockPush).toHaveBeenCalledTimes(1);
+  });
+
+  it('should show loader while loading data in context', () => {
+    render(<Home />, { value: { ...mockValue, loading: true } });
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+  });
+
+  it('should redirect to home route if user is logged in', () => {
+    render(<Home />, { value: { ...mockValue, user: mockUser } });
+    expect(mockReplace).toHaveBeenCalled();
+    expect(mockReplace).toHaveBeenCalledTimes(1);
+    expect(mockReplace).toHaveBeenCalledWith(Routes.HOME_SCREEN);
   });
 });
