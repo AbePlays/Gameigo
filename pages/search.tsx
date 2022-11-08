0957d7dc1cf2d89f2b91997538f0f73aeb80ce9b
Abhishek Rawat <abhi.rawat456@gmail.com>
2022-11-08 12:50:20 +0530

    feat: prerender next page results

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent, useRef, useState } from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { Box, Heading, InputGroup, InputLeftElement, Text } from '@chakra-ui/react';
+import { useRef, useState } from 'react';
 
 import BlurImage from '@components/BlurImage';
 import Input from '@components/Input';
@@ -8,7 +9,7 @@ import Page from '@containers/Page';
 import img from 'public/images/search_hero.jpeg';
 import { Descriptions } from 'seo';
 
-const Search: FunctionComponent = () => {
+export default function Search() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [page, setPage] = useState<number>(1);
   const [query, setQuery] = useState<string>('');
@@ -25,55 +26,47 @@ const Search: FunctionComponent = () => {
   const incrementPage = () => setPage((prev) => prev + 1);
 
   return (
-    <Page
-      title="Search"
-      description={Descriptions.Search}
-      maxWidth="1536px"
-      px="0"
-      pt="0"
-    >
+    <Page title="Search" description={Descriptions.Search} maxWidth="1536px" px="0" pt="0">
       <Box position="relative">
-        <Box height={['200px', '300px']} overflow="hidden" position="relative">
-          <BlurImage
-            alt=""
-            style={{ objectPosition: 'top' }}
-            showBg
-            src={img}
-          />
+        {/* Background Image */}
+        <Box height={['200px', '300px']} overflow="hidden" pos="relative">
+          <BlurImage alt="" fill style={{ objectPosition: 'top' }} showBg src={img} />
         </Box>
-        <Box maxW="container.sm" mx="auto" px="4" transform="translateY(-30px)">
-          <Input
-            ref={inputRef}
-            onKeyDown={keyDownHandler}
-            height="60px"
-            placeholder="Search Games"
-          />
-        </Box>
-        <Box
-          color="light-text"
-          textAlign="center"
-          px="4"
-          position="absolute"
-          width="100%"
-          left="0"
-          top={[8, 16]}
-        >
-          <Heading>Search Gameigo</Heading>
-          <Text mt={[1, 4]}>
+
+        {/* Intro Text */}
+        <Box color="light-text" textAlign="center" px="4" position="absolute" width="100%" left="0" top={[8, 16]}>
+          <Heading as="h1">Search Gameigo</Heading>
+          <Text fontWeight="medium" mt={[1, 4]}>
             Choose from 30,000+ games with new addition every few weeks.
           </Text>
         </Box>
+
+        {/* Search Input */}
+        <Box maxW="container.sm" mx="auto" px="4" transform="translateY(-25px)">
+          <InputGroup>
+            <InputLeftElement height="100%" pl="2" pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              height="50px"
+              onKeyDown={keyDownHandler}
+              paddingLeft="10"
+              placeholder="Search Games"
+              ref={inputRef}
+            />
+          </InputGroup>
+        </Box>
       </Box>
+
+      {/* Search Results */}
+      {query ? <SearchResult query={query} page={page} onNext={incrementPage} onPrevious={decrementPage} /> : null}
+
+      {/* For preloading next page */}
       {query ? (
-        <SearchResult
-          query={query}
-          page={page}
-          onNext={incrementPage}
-          onPrevious={decrementPage}
-        />
+        <Box display="none">
+          <SearchResult query={query} page={page + 1} onNext={incrementPage} onPrevious={decrementPage} />
+        </Box>
       ) : null}
     </Page>
   );
-};
-
-export default Search;
+}
