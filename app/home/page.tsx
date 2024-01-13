b4a0dd1d44d39549bc89c5de5c2aedbf3020d516
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-01-13 23:36:29 +0530

    feat: parse data using valibot

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,11 @@
 import { Container, Heading, Link, Text } from '@radix-ui/themes';
+import { type Metadata } from 'next';
 import NextLink from 'next/link';
+import { parse } from 'valibot';
 
 import GameCard from '@components/GameCard';
-import { convertToGame } from '@utils/game';
-import { Endpoints } from 'endpoints';
-import { type Metadata } from 'next';
+import { getSixMonthsAgoDate, getTodaysDate } from '@utils/date';
+import { Game, GameSearchSchema } from 'schemas/game';
 
 export const metadata: Metadata = {
   title: 'Gameigo | Home',
@@ -13,29 +14,31 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  const res = await fetch(Endpoints.TRENDING_GAMES);
-  const data = await res.json();
   let games: Game[] = [];
-  if (data.results && Array.isArray(data.results)) {
-    games = data.results.map((game) => convertToGame(game));
+  try {
+    const res = await fetch(
+      `https://api.rawg.io/api/games?key=${
+        process.env.NEXT_PUBLIC_RAWG_API_KEY
+      }&dates=${getSixMonthsAgoDate()},${getTodaysDate()}&ordering=-added`
+    );
+    const data = parse(GameSearchSchema, await res.json());
+    games = data.results;
+  } catch (e) {
+    console.error(JSON.stringify(e));
   }
 
   return (
     <Container>
-      <Heading as="h1" size="8">
-        New and trending
-      </Heading>
+      <Heading size="8">New and trending</Heading>
       <Text>Based on player counts and release date</Text>
       <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8 mt-8">
-        {Array.isArray(games) &&
-          games.length > 0 &&
-          games.map((game) => (
-            <Link asChild aria-label={game.name} key={game.id}>
-              <NextLink href={`/game/${game.slug}`}>
-                <GameCard game={game} />
-              </NextLink>
-            </Link>
-          ))}
+        {games.map((game) => (
+          <Link asChild aria-label={game.name} key={game.id}>
+            <NextLink href={`/game/${game.slug}`}>
+              <GameCard game={game} />
+            </NextLink>
+          </Link>
+        ))}
       </div>
     </Container>
   );
