6f107ba7d973a7e32cc99980e85230c9355e9381
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-01-14 12:10:09 +0530

    chore: remove pages artifacts

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -1,16 +1,13 @@
-import { FunctionComponent } from 'react';
-import { Center } from '@chakra-ui/react';
+import { Box, Flex } from '@radix-ui/themes';
 
-const Loader: FunctionComponent = () => {
+export function Loader() {
   return (
-    <Center mx="4" my="16">
-      <div className="loader" data-testid="loader">
+    <Flex align="center" justify="center" mx="4" my="8">
+      <Box className="loader" data-testid="loader">
         <svg viewBox="0 0 80 80">
           <circle id="test" cx="40" cy="40" r="32"></circle>
         </svg>
-      </div>
-    </Center>
+      </Box>
+    </Flex>
   );
-};
-
-export default Loader;
+}
