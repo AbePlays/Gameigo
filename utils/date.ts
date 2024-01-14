7c9302e9794ab0915b4838efa8dfa78714fc0eae
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-01-14 17:11:14 +0530

    refactor: remove more code

--- utils/date.ts
+++ utils/date.ts
@@ -1,22 +1,22 @@
-export const getTodaysDate = (): string => {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0');
-  const yyyy = today.getFullYear();
-  return `${yyyy}-${mm}-${dd}`;
-};
+export function formatDate(releasedDate: string) {
+  const date = new Date(releasedDate).toDateString();
+  const arr = date.split(' ');
+  return arr[1] + ' ' + arr[2] + ', ' + arr[3];
+}
 
-export const getSixMonthsAgoDate = (): string => {
+export function getSixMonthsAgoDate() {
   const sixMonthsAgo = new Date();
   sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
   const dd = String(sixMonthsAgo.getDate()).padStart(2, '0');
   const mm = String(sixMonthsAgo.getMonth() + 1).padStart(2, '0');
   const yyyy = sixMonthsAgo.getFullYear();
   return `${yyyy}-${mm}-${dd}`;
-};
+}
 
-export const formatDate = (releasedDate: string): string => {
-  const date = new Date(releasedDate).toDateString();
-  const arr = date.split(' ');
-  return arr[1] + ' ' + arr[2] + ', ' + arr[3];
-};
+export function getTodaysDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+}
